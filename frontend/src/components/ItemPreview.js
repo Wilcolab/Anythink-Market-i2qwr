4f3912db28fe5c3a5066f4a5056d78a70c9639f2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-31 14:27:27 +0000

    restart

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,9 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
-
   // item.image = item.image || '/placeholder.png';
-
   const handleClick = (ev) => {
     ev.preventDefault();
     if (item.favorited) {
