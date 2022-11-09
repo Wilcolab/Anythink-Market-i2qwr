60b1c7db21fdde9eb08e5bfedf70353ceaa98f2a
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-11-09 10:03:32 +0000

    Update class component to functional

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,8 +49,7 @@ const LoggedInView = (props) => {
   );
 };
 
-class Header extends React.Component {
-  render() {
+const Header = (props) => {
     return (
       <nav
         className="navbar navbar-expand-md navbar-dark"
@@ -60,14 +59,34 @@ class Header extends React.Component {
           <img alt="logo" src={logo} />
         </Link>
 
-        {this.props.currentUser ? (
-          <LoggedInView currentUser={this.props.currentUser} />
+        {props.currentUser ? (
+          <LoggedInView currentUser={props.currentUser} />
         ) : (
-          <LoggedOutView currentUser={this.props.currentUser} />
+          <LoggedOutView currentUser={props.currentUser} />
         )}
       </nav>
     );
-  }
 }
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <nav
+//         className="navbar navbar-expand-md navbar-dark"
+//         style={{ padding: "0.5rem 2rem" }}
+//       >
+//         <Link to="/" className="navbar-brand">
+//           <img alt="logo" src={logo} />
+//         </Link>
+
+//         {this.props.currentUser ? (
+//           <LoggedInView currentUser={this.props.currentUser} />
+//         ) : (
+//           <LoggedOutView currentUser={this.props.currentUser} />
+//         )}
+//       </nav>
+//     );
+//   }
+// }
 
 export default Header;
