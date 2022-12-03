dce0983972da8f9605e848ca9189a2d4f456436d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-12-03 15:21:42 +0000

    Fix linters

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,24 +49,23 @@ const LoggedInView = (props) => {
   );
 };
 
-const Header = (props) => {  
-    return (
-      <nav
-        className="navbar navbar-expand-md navbar-dark"
-        style={{ padding: "0.5rem 2rem" }}
-      >
-        <Link to="/" className="navbar-brand">
-          <img alt="logo" src={logo} />
-        </Link>
+const Header = (props) => {
+  return (
+    <nav
+      className="navbar navbar-expand-md navbar-dark"
+      style={{ padding: "0.5rem 2rem" }}
+    >
+      <Link to="/" className="navbar-brand">
+        <img alt="logo" src={logo} />
+      </Link>
 
-        {props.currentUser ? (
-          <LoggedInView currentUser={props.currentUser} />
-        ) : (
-          <LoggedOutView currentUser={props.currentUser} />
-        )}
-      </nav>
-    );
-
-}
+      {props.currentUser ? (
+        <LoggedInView currentUser={props.currentUser} />
+      ) : (
+        <LoggedOutView currentUser={props.currentUser} />
+      )}
+    </nav>
+  );
+};
 
 export default Header;
